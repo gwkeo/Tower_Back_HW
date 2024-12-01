4372ba0bd276e24c17d2ade7ec821e0a6bbfcaef
Rakhman Imanov <raxman.i@mail.ru>
2024-12-01 09:21:23 +0300

    Исправлены названия тестов на более понятные и корректные

--- hw3/uniq/uniq_test.go
+++ hw3/uniq/uniq_test.go
@@ -27,12 +27,12 @@ func TestUniq(t *testing.T) {
 	}{
 		{"Same 3 lines with no flags", threeSameLines, defaultFlags, "cake\n"},
 		{"Different 3 lines with no flags", differentFirstChar, defaultFlags, "cake\nbake\ntake\n"},
-		{"3 lines with different first chars with differentFirstChar flag", differentFirstChar, skipCharFlag, "cake\n"},
-		{"3 lines with different first fields with differentFirstField flag", differentFirstField, skipFieldFlag, "I met\n"},
+		{"3 lines with different first chars with skipChar flag", differentFirstChar, skipCharFlag, "cake\n"},
+		{"3 lines with different first fields with skipField flag", differentFirstField, skipFieldFlag, "I met\n"},
 		{"3 lines with random case with ignoreCase flag", differentCase, ignoreCaseFlag, "cAke\n"},
 		{"3 lines with two same and one unique with countSame flag", sameAndUniqueLines, countSameFlag, "2 cake\n1 bake\n"},
-		{"3 lines with two same and one unique with uniqueOnly flag", sameAndUniqueLines, uniqueOnlyFlag, "cake\n"},
-		{"3 lines with two same and one unique with sameOnly flag", sameAndUniqueLines, sameOnlyFlag, "bake\n"},
+		{"3 lines with two same and one unique with sameOnly flag", sameAndUniqueLines, sameOnlyFlag, "cake\n"},
+		{"3 lines with two same and one unique with uniqueOnly flag", sameAndUniqueLines, uniqueOnlyFlag, "bake\n"},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
