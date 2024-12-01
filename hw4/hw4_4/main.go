75af9afb1903861614d575381014234d6fda58f4
Rakhman Imanov <raxman.i@mail.ru>
2024-12-01 11:31:05 +0300

    Сделано 4 задание

--- hw4/hw4_4/main.go
+++ hw4/hw4_4/main.go
@@ -2,58 +2,59 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"math/rand"
-	"os"
+	"log"
 	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 )
 
-const (
-	workersNum      = 3
-	jobsArrangement = 100
-)
+const workersNum = 3
 
-func worker(id int, jobs <-chan int, results chan<- int) {
-	for j := range jobs {
-		fmt.Println("worker", id, "started job", j)
-		time.Sleep(time.Second)
-		fmt.Println("worker", id, "finished job", j)
-		results <- j
+func worker(ctx context.Context, wg *sync.WaitGroup, id int, jobs <-chan int) {
+	defer wg.Done()
+	for {
+		select {
+		case <-ctx.Done():
+			log.Printf("worker %d stopped\n", id)
+			return
+		case job, ok := <-jobs:
+			if !ok {
+				log.Printf("worker %d stopped\n", id)
+			}
+			log.Printf("worker %d doing job %d\n", id, job)
+			time.Sleep(1 * time.Second)
+			log.Printf("worker %d done job %d\n", id, job)
+		}
 	}
 }
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
 	jobs := make(chan int)
-	results := make(chan int)
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
 
 	go func() {
+		k := 0
 		for {
 			select {
 			case <-ctx.Done():
-				fmt.Println("context canceled")
 				return
 			default:
-				jobs <- rand.Intn(jobsArrangement)
+				jobs <- k
+				k++
+				time.Sleep(500 * time.Millisecond)
 			}
 		}
 	}()
 
-	go func() {
-		for i := 0; i < workersNum; i++ {
-			select {
-			case <-c:
-				cancel()
-			default:
-				go worker(i, jobs, results)
-			}
-		}
-	}()
-	time.Sleep(time.Second)
+	var wg sync.WaitGroup
+	for i := 0; i < workersNum; i++ {
+		wg.Add(1)
+		go worker(ctx, &wg, i, jobs)
+	}
+
+	wg.Wait()
+	log.Println("all workers has done their jobs, shutting down... ")
 }
